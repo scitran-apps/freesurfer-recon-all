dd6351a6a1397b4a665e537947d48c05d4384c18
Mengxing Liu <lmx@black>
2020-05-10 13:54:00 -0700

    added HCP atlas, update ANTs Registration~

--- bin/parse_config.py
+++ bin/parse_config.py
@@ -77,9 +77,15 @@ def parse_config(args):
     # Process thalamic nuclei
     if args.t:
         print(config['config']['thalamic_nuclei'])
+
+    # Process HCP atlas
+    if args.g:
+        print(config['config']['hcp'])
+
     # Process neuropythy commands    
     if args.p:
         print(config['config']['neuropythy_analysis'])
+
     # separate aparcaseg ROIs
     if args.aparc2009:
         print(config['config']['aparc2009'])
@@ -115,6 +121,7 @@ if __name__ == '__main__':
     ap.add_argument('-t', action='store_true', help='Thalamus processing')
     ap.add_argument('-e', action='store_true', help='cerebellum processing')
     ap.add_argument('-m', action='store_true', help='transform mori rois')
+    ap.add_argument('-g', action='store_true', help='HCP atlas processing')
     ap.add_argument('-p', action='store_true', help='Neuropythy processing')
     ap.add_argument('-r', action='store_true', help='Surface registration')
     ap.add_argument('-z', action='store_true', help='Get sub code from zip input')
--- bin/separateROIs.py
+++ bin/separateROIs.py
@@ -53,6 +53,33 @@ def segThalamus():
             print(cmdstr)
             sp.call(cmdstr, shell=True) 
 
+def segHCP():
+    import os
+    import subprocess as sp
+    fLUT = open(args.hcpLUT)
+    LUT = fLUT.readlines()
+    fLUT.close()
+    cleanLUT = [s.rstrip('\n') for s in LUT if not '#' in s]
+    # Obtain the labels of thalamic nuclei
+    index  = [int(s.split()[0]) for s in cleanLUT]
+    label  = [str(s.split()[1]) for s in cleanLUT]
+    (head, tail) = os.path.split(args.hcp)
+    # dilstr = ['', '_dil-1', '_dil-2']; diloption = ['', '-dilate 1', '-dilate 2']
+    dilstr = ['']; diloption = ['']
+    for i in range(len(index)):
+        # extract nuclei
+        for x in range(len(dilstr)):
+            roiname = os.path.join(head, 'ROIs', str(label[i] + dilstr[x] + '.nii.gz'))
+            cmdstr = str('mri_extract_label ' + diloption[x] + ' ' + args.hcp + ' ' +
+                str(index[i]) + ' ' + roiname)
+            print(cmdstr)
+            sp.call(cmdstr, shell=True)
+            # binarize the ROIs
+            cmdstr = str('mri_binarize ' + '--min 0.1 ' + '--i ' + roiname + ' ' + '--o ' + roiname)
+            print(cmdstr)
+            sp.call(cmdstr, shell=True)
+
+
 def segBensonVarea():
     import os
     import subprocess as sp
@@ -160,6 +187,9 @@ def separateROIs(args):
     if args.ThN:
         print('separating thalamic nuclei')
         segThalamus()
+    if args.hcp:
+        print('separating HCP atlas')
+        segHCP()
     if args.aparc2009:
         print('separating aparc+aseg.nii.gz')
         segAparc2009()
